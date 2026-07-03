569199cf0dd639d8ff0a55c3c5ebd016fb2f88fd
xxytan <ufxmds@gmail.com>
2026-07-03 12:43:17 +0800

    again

--- config/site.config.js
+++ config/site.config.js
@@ -41,6 +41,7 @@ module.exports = {
   // the directories in which you have .password set. Check the documentation for details.
   protectedRoutes: [
     '/PRIVATE',
+    '/190memories',
   ],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
